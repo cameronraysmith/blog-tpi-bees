9eb24104f082540294950c0c9f2dbef9b9bbfea6
Cameron Smith <cameron.ray.smith@gmail.com>
2023-01-22 00:07:26 -0500

    use gcp instead of aws

--- main.tf
+++ main.tf
@@ -3,34 +3,13 @@ terraform {
 }
 provider "iterative" {}
 
-# Basic (CPU) version
-resource "iterative_task" "example-basic" {
-  cloud   = "aws"    # or any of: gcp, az, k8s
-  machine = "m"      # medium. Or any of: l, xl, m+k80, xl+v100, ...
-  spot    = 0        # auto-price. Default -1 to disable, or >0 for hourly USD limit
-  timeout = 24*60*60 # 24h
-  image   = "ubuntu"
-
-  storage {
-    workdir = "src"
-    output  = "results-basic"
-  }
-  environment = { TF_CPP_MIN_LOG_LEVEL = "1" }
-  script = <<-END
-    #!/bin/bash
-    sudo apt-get update -q
-    sudo apt-get install -yq python3-pip
-    pip3 install -r requirements.txt tensorflow-cpu==2.8.0
-    python3 train.py --output results-basic/metrics.json
-  END
-}
-
 # GPU version
 resource "iterative_task" "example-gpu" {
-  cloud   = "aws"
-  machine = "m+t4"   # 4 CPUs and an NVIDIA Tesla T4 GPU
+  cloud   = "gcp"
+  machine = "m+t4" # 4 CPUs and an NVIDIA Tesla T4 GPU
+  region  = var.region
   spot    = 0
-  timeout = 24*60*60
+  timeout = 24 * 60 * 60
   image   = "nvidia" # has CUDA GPU drivers
 
   storage {
@@ -38,7 +17,7 @@ resource "iterative_task" "example-gpu" {
     output  = "results-gpu"
   }
   environment = { TF_CPP_MIN_LOG_LEVEL = "1" }
-  script = <<-END
+  script      = <<-END
     #!/bin/bash
     sudo apt-get update -q
     sudo apt-get install -yq python3-pip
@@ -46,3 +25,39 @@ resource "iterative_task" "example-gpu" {
     python3 train.py --output results-gpu/metrics.json
   END
 }
+
+# output "gpu-logs" {
+#   value = try(join("\n", iterative_task.example-gpu.logs), "")
+# }
+
+
+# CPU version
+# resource "iterative_task" "example-cpu" {
+#   cloud   = "gcp" # or any of: gcp, az, k8s
+#   machine = "m"   # medium. Or any of: l, xl, m+k80, xl+v100, ...
+#   region  = var.region
+#   spot    = 0            # auto-price. Default -1 to disable, or >0 for hourly USD limit
+#   timeout = 24 * 60 * 60 # 24h
+#   image   = "ubuntu"
+
+#   storage {
+#     workdir = "src"
+#     output  = "results-basic"
+#   }
+#   environment = { TF_CPP_MIN_LOG_LEVEL = "1" }
+#   script      = <<-END
+#     #!/bin/bash
+#     sudo apt-get update -q
+#     sudo apt-get install -yq python3-pip
+#     pip3 install -r requirements.txt tensorflow-cpu==2.8.0
+#     python3 train.py --output results-basic/metrics.json
+#   END
+# }
+
+# output "cpu-logs" {
+#   value = try(join("\n", iterative_task.example-cpu.logs), "")
+# }
+
+
+
+
